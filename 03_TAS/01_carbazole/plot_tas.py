2ebe378711ad83567a3bd48264727b8ea286d600
Franco Bonafe <fbonafe@unc.edu.ar>
2022-06-01 13:45:53 +0200

    Add TAS files

---
+++ 03_TAS/01_carbazole/plot_tas.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import constants
+import os
+
+def loadSpecs(ROOT, nphases):
+    delay_time = np.genfromtxt(ROOT+'delaytime.dat')
+    freq = np.genfromtxt(ROOT+'energy.dat')
+    spec0 = np.genfromtxt(ROOT+'gs_spectrum.dat')
+    specs = []
+    for ph in range(nphases):
+        specs.append(np.genfromtxt(ROOT+'spectra_ph{}.dat'.format(ph)))
+    return freq, delay_time, specs, spec0
+
+class specData():
+    def __init__(self, ROOT, nphases):
+        en, dlt, spec, spec0 = loadSpecs(ROOT, nphases)
+        self.energies = en
+        self.delaytime = dlt
+        self.spec = spec
+        self.spec0 = spec0
+        self.nphases = nphases
+        self.avespec = sum(self.spec[:])/self.nphases
+        self.tas = self.avespec - self.spec0
+        
+def plotTasAxis(axx, specs, freq, delaytime, vmax=None):
+    im = axx.imshow(specs, aspect='auto', interpolation='hanning',
+              cmap=plt.get_cmap('jet'), extent=[freq.min(), freq.max(), delaytime.max(), delaytime.min()])
+    axx.set_xlabel('energy / eV')
+    axx.set_ylabel('delay time / fs')
+    return im 
+
+tas_data = specData('./', 4)
+plt.figure()
+plt.plot(tas_data.energies, tas_data.spec0)
+plt.title('Ground state spectrum')
+plt.savefig('gs_spectrum.png' , dpi=200)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(9, 8))
+im = plotTasAxis(ax, tas_data.avespec, tas_data.energies, tas_data.delaytime)
+fig.colorbar(im, ax=ax)
+ax.axvline(3.80, c='w', lw=3)
+ax.set_xlim(1.0, 8.0)
+plt.savefig('tas.png' , dpi=200)
+plt.show()
+
---
+++ 03_TAS/01_carbazole/run-probes.sh
@@ -0,0 +1,49 @@
+pumpdirs=$(ls -d phase_*)
+
+DFTB_PATH=~/dftb_timeprop/_build/_install/bin/dftb+ 
+nFrames=$(ls phase_0/pump_frames/*ppdump.bin | wc -l)
+let nOk=$nFrames-1
+
+echo $nFrames "frames will be used"
+
+#files for GS spectrum
+mkdir gs_spectrum
+cp dftb_in.hsd_probe gs_spectrum/dftb_in.hsd
+cp coords.gen phase_0/charges.bin gs_spectrum
+
+#files for probes
+for dir in $pumpdirs; do
+cd $dir
+mkdir probes
+
+for i in $(seq 0 $nOk); do
+  mkdir probes/frame$i
+  cp ../dftb_in.hsd_probe probes/frame$i/dftb_in.hsd
+  cp coords.gen charges.bin probes/frame$i
+  cp pump_frames/${i}ppdump.bin probes/frame$i/tddump.bin
+done
+
+cd ..
+done
+
+echo "Running GS spectrum"
+#run GS spectrum
+cd gs_spectrum
+sed -i '/[[:blank:]]Probe = Yes/d' dftb_in.hsd
+$DFTB_PATH >& out.log
+cd ..
+
+echo "Done GS spectrum"
+echo "Running probes...."
+#run probes
+export OMP_NUM_THREADS=1
+for dir in $pumpdirs; do
+echo "Running probes in" $dir
+cd $dir/probes
+for ff in frame*; do
+  cd $ff
+  $DFTB_PATH >& out.log
+  cd ..
+done
+cd ../..
+done
